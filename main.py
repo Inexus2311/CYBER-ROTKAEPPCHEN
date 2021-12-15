62962d681156f2a373fb6558c9526bddcba1f051
Adrian Höppener <adrian.hoeppener@alumni.fh-aachen.de>
2021-12-15 23:39:58 +0100

    Task Solution Update Changes

--- main.py
+++ main.py
@@ -1,50 +1,52 @@
-import re
+# import re
 
 datei = open('cybertext.txt', 'r')
 string = datei.read().splitlines()
 NewText = list()
 cyber_binary = ""
 count_index = 0
-num_block = 0 # 1 Byte-Block counter
+num_block = 0  # 1 Byte-Block counter
 
 
 # Kontrolliere cyber_binary Byte für Byte
 
 def check_Byte(txt):
     count = 0
-    for i in txt:
-        count+=1
-        if count ==8:
+    for _ in txt:
+        count += 1
+        if count == 8:
             return txt
-    while count <8:
+    while count < 8:
         txt += str(0)
         count += 1
     return txt
+
 
 # Function Binary to Ascii-char output
 def to_ascii(char):
     return chr(int(char, 2))
 
+
 # Translate one Byte-block of binaries
 def translate(txt):
-    result = ''
+    result_tmp: str = ''
     letters = txt.split(' ')
     for letter in letters:
         byte_var = check_Byte(letter)
-        result += to_ascii(byte_var)
-    return result
+        result_tmp += to_ascii(byte_var)
+    return result_tmp
+
 
 # Split lines in Text
 for lines in string:
     if lines.__len__() > 0:
         NewText.append(lines.split())
 
-
 # Jedes Wort aus Text einzeln lesen
 for line in NewText:
     for i in line:
         if count_index <= 8:
-            if  i.__contains__("Cyber-") or i.__contains__("cyber-"):
+            if i.__contains__("Cyber-") or i.__contains__("cyber-"):
                 cyber_binary += str(1)
                 count_index = count_index + 1
             elif i.__contains__("Cyber") or i.__contains__("cyber"):
@@ -62,5 +64,4 @@ result = translate(cyber_binary)
 print("Das Lösungswort lautet: " + str(result))
 print("done")
 
-
-datei.close()
+datei.close() # Close Text-datafile
